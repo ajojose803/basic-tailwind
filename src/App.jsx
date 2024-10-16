7483133fedd251a5121d334a64781befc77b8cdf
ajojose803 <ajojose803@gmail.com>
2024-10-16 23:08:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{display: "flex", justifyContent: "space-around"}}> 
+    <div style={{backgroundColor: "red"}}> Red</div>
+    <div style={{backgroundColor: "green"}}> Green</div>
+    <div style={{backgroundColor: "red"}}> Red</div>
+    <div style={{backgroundColor: "yellow"}}> Yellow</div>
+    </div>
+  )
+}
